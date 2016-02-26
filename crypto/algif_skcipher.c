ff82ff60dd9f27d21450b400f07e91fcd31f5545
Herbert Xu <herbert@gondor.apana.org.au>
2016-02-26 12:44:08 +0100

    Backport fix for crypto: algif_skcipher - Require setkey before accept(2)
    commit dd504589577d8e8e70f51f997ad487a4cb6c026f upstream.
    
    Some cipher implementations will crash if you try to use them
    without calling setkey first.  This patch adds a check so that
    the accept(2) call will fail with -ENOKEY if setkey hasn't been
    done on the socket yet.
    
    Cc: stable@vger.kernel.org
    Reported-by: Dmitry Vyukov <dvyukov@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Tested-by: Dmitry Vyukov <dvyukov@google.com>
    [backported to 4.1 by Milan Broz <gmazyland@gmail.com>]
    
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- crypto/algif_skcipher.c
+++ crypto/algif_skcipher.c
@@ -755,102 +755,6 @@ static struct proto_ops algif_skcipher_ops = {
 	.poll		=	skcipher_poll,
 };
 
-static int skcipher_check_key(struct socket *sock)
-{
-	int err = 0;
-	struct sock *psk;
-	struct alg_sock *pask;
-	struct skcipher_tfm *tfm;
-	struct sock *sk = sock->sk;
-	struct alg_sock *ask = alg_sk(sk);
-
-	lock_sock(sk);
-	if (ask->refcnt)
-		goto unlock_child;
-
-	psk = ask->parent;
-	pask = alg_sk(ask->parent);
-	tfm = pask->private;
-
-	err = -ENOKEY;
-	lock_sock_nested(psk, SINGLE_DEPTH_NESTING);
-	if (!tfm->has_key)
-		goto unlock;
-
-	if (!pask->refcnt++)
-		sock_hold(psk);
-
-	ask->refcnt = 1;
-	sock_put(psk);
-
-	err = 0;
-
-unlock:
-	release_sock(psk);
-unlock_child:
-	release_sock(sk);
-
-	return err;
-}
-
-static int skcipher_sendmsg_nokey(struct socket *sock, struct msghdr *msg,
-				  size_t size)
-{
-	int err;
-
-	err = skcipher_check_key(sock);
-	if (err)
-		return err;
-
-	return skcipher_sendmsg(sock, msg, size);
-}
-
-static ssize_t skcipher_sendpage_nokey(struct socket *sock, struct page *page,
-				       int offset, size_t size, int flags)
-{
-	int err;
-
-	err = skcipher_check_key(sock);
-	if (err)
-		return err;
-
-	return skcipher_sendpage(sock, page, offset, size, flags);
-}
-
-static int skcipher_recvmsg_nokey(struct socket *sock, struct msghdr *msg,
-				  size_t ignored, int flags)
-{
-	int err;
-
-	err = skcipher_check_key(sock);
-	if (err)
-		return err;
-
-	return skcipher_recvmsg(sock, msg, ignored, flags);
-}
-
-static struct proto_ops algif_skcipher_ops_nokey = {
-	.family		=	PF_ALG,
-
-	.connect	=	sock_no_connect,
-	.socketpair	=	sock_no_socketpair,
-	.getname	=	sock_no_getname,
-	.ioctl		=	sock_no_ioctl,
-	.listen		=	sock_no_listen,
-	.shutdown	=	sock_no_shutdown,
-	.getsockopt	=	sock_no_getsockopt,
-	.mmap		=	sock_no_mmap,
-	.bind		=	sock_no_bind,
-	.accept		=	sock_no_accept,
-	.setsockopt	=	sock_no_setsockopt,
-
-	.release	=	af_alg_release,
-	.sendmsg	=	skcipher_sendmsg_nokey,
-	.sendpage	=	skcipher_sendpage_nokey,
-	.recvmsg	=	skcipher_recvmsg_nokey,
-	.poll		=	skcipher_poll,
-};
-
 static void *skcipher_bind(const char *name, u32 type, u32 mask)
 {
 	struct skcipher_tfm *tfm;
@@ -915,13 +819,16 @@ static void skcipher_sock_destruct(struct sock *sk)
 	af_alg_release_parent(sk);
 }
 
-static int skcipher_accept_parent_nokey(void *private, struct sock *sk)
+static int skcipher_accept_parent(void *private, struct sock *sk)
 {
 	struct skcipher_ctx *ctx;
 	struct alg_sock *ask = alg_sk(sk);
 	struct skcipher_tfm *tfm = private;
 	struct crypto_ablkcipher *skcipher = tfm->skcipher;
 	unsigned int len = sizeof(*ctx) + crypto_ablkcipher_reqsize(skcipher);
+
+	if (!tfm->has_key)
+		return -ENOKEY;
 
 	ctx = sock_kmalloc(sk, len, GFP_KERNEL);
 	if (!ctx)
@@ -956,24 +863,12 @@ static int skcipher_accept_parent_nokey(void *private, struct sock *sk)
 	return 0;
 }
 
-static int skcipher_accept_parent(void *private, struct sock *sk)
-{
-	struct skcipher_tfm *tfm = private;
-
-	if (!tfm->has_key)
-		return -ENOKEY;
-
-	return skcipher_accept_parent_nokey(private, sk);
-}
-
 static const struct af_alg_type algif_type_skcipher = {
 	.bind		=	skcipher_bind,
 	.release	=	skcipher_release,
 	.setkey		=	skcipher_setkey,
 	.accept		=	skcipher_accept_parent,
-	.accept_nokey	=	skcipher_accept_parent_nokey,
 	.ops		=	&algif_skcipher_ops,
-	.ops_nokey	=	&algif_skcipher_ops_nokey,
 	.name		=	"skcipher",
 	.owner		=	THIS_MODULE
 };
